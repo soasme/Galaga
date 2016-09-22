a21f425ce52711dcc9f5000ff32a0c0e1745c023
soasme <soasme@gmail.com>
2016-09-22 21:43:13 +0800

    set army position.

--- Assets/Scripts/EnemyArmySpawner.cs
+++ Assets/Scripts/EnemyArmySpawner.cs
@@ -5,6 +5,8 @@ public class EnemyArmySpawner : MonoBehaviour {
 
 	public int enemyRows = 3;
 	public int enemyColumns = 10;
+	public int direction = 1;
+	public float speed = 1;
 	public GameObject enemyPref;
 	public GameObject[] enemies;
 
@@ -22,15 +24,30 @@ public class EnemyArmySpawner : MonoBehaviour {
 		Vector2 center = gameObject.transform.position;
 		for (int i = 0; i < enemyRows; i++) {
 			for (int j = 0; j < enemyColumns; j++) {				
-				Vector2 pos = new Vector2 (j - enemyColumns / 2, i);
+				Vector2 pos = new Vector2 (j - enemyColumns / 2 + 0.5f, i);
 				enemies [i] = (GameObject)Instantiate (enemyPref, pos, Quaternion.identity);
+				enemies [i].transform.SetParent (this.transform);
 			}
+		}
+	}
+
+	void Move() {
+		var pos = transform.position;
+		pos.x = pos.x + direction * Time.deltaTime * speed;
+		pos.x = Mathf.Clamp (pos.x, -1, 1);
+
+		transform.position = pos;
+
+		if (pos.x == -1 || pos.x == 1) {
+			direction = -1 * direction;
 		}
 	}
 
 	void Update () {
 		if (NeedSpawn ()) {
 			Spawn ();
+		} else {
+			Move ();
 		}
 	}
 }
