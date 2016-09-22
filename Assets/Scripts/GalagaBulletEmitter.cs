886861eb9643d7968daa7a43fed88fdc79319af0
soasme <soasme@gmail.com>
2016-09-22 20:30:33 +0800

    Define GalagaBulletEmitter.

---
+++ Assets/Scripts/GalagaBulletEmitter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GalagaBulletEmitter : MonoBehaviour {
+
+	// emit bullet from this object
+	public GameObject bulletEmitter;
+
+	// bullet prefab referrence
+	public GameObject bulletPref;
+
+	// bullet fire speed
+	public float bulletSpeedScale;
+
+	void Fire() {
+		GameObject bullet = (GameObject) Instantiate (bulletPref, bulletEmitter.transform.position, Quaternion.identity);
+		Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D> ();
+		rigidbody.AddForce (Vector2.up * bulletSpeedScale);
+		Destroy (bullet, 10.0f);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Fire ();
+		}
+	}
+}
