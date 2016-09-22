31ac0cb64088a00ef42d561f0c7309c9cb5d442d
soasme <soasme@gmail.com>
2016-09-22 21:21:24 +0800

    add bee army.

---
+++ Assets/Scripts/EnemyArmySpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyArmySpawner : MonoBehaviour {
+
+	public int enemyRows = 3;
+	public int enemyColumns = 10;
+	public GameObject enemyPref;
+	public GameObject[] enemies;
+
+	void Start () {
+		enemies = new GameObject[enemyRows * enemyColumns];
+
+		Spawn ();
+	}
+
+	bool NeedSpawn() {
+		return enemies.Length == 0;
+	}
+
+	void Spawn() {
+		Vector2 center = gameObject.transform.position;
+		for (int i = 0; i < enemyRows; i++) {
+			for (int j = 0; j < enemyColumns; j++) {				
+				Vector2 pos = new Vector2 (j - enemyColumns / 2, i);
+				enemies [i] = (GameObject)Instantiate (enemyPref, pos, Quaternion.identity);
+			}
+		}
+	}
+
+	void Update () {
+		if (NeedSpawn ()) {
+			Spawn ();
+		}
+	}
+}
