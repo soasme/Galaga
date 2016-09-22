a5feb55a11a6aee875cacb983c2fc17c6c86123c
soasme <soasme@gmail.com>
2016-09-23 07:46:50 +0800

    Explode galaga on trigger entered.

--- Assets/Scripts/GalagaLife.cs
+++ Assets/Scripts/GalagaLife.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class GalagaLife : MonoBehaviour {
 
+	public GameObject explosionPref;
+
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.tag == "Bullet" || collider.tag == "Enemy") {
+			PlayExplosion ();
 			Destroy (gameObject);
 		}
+	}
+
+	void PlayExplosion(){
+		var explosion = (GameObject)Instantiate (explosionPref);
+		explosion.transform.position = gameObject.transform.position;
 	}
 }
