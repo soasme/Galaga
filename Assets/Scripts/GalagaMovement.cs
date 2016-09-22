3fed4d30c0e0dfa381a9936d4f7f158f0fef665e
soasme <soasme@gmail.com>
2016-09-22 19:31:48 +0800

    move galaga.

---
+++ Assets/Scripts/GalagaMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GalagaMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void Update () {
+		var position = gameObject.transform.position;
+		var x = position.x + Input.GetAxis ("Horizontal") * speed;
+		var y = position.y + Input.GetAxis ("Vertical") * speed;
+		gameObject.transform.position = new Vector2 (x, y);
+	}
+}
