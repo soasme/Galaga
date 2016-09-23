c2162bb182c442ff865270e0e5c8882b696a8438
soasme <soasme@gmail.com>
2016-09-23 14:34:52 +0800

    Extract bullet emitter base class.

---
+++ Assets/Scripts/BulletEmitter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletEmitter : MonoBehaviour {
+
+	// emit bullet from this object
+	public GameObject bulletEmitter;
+
+	// bullet prefab referrence
+	public GameObject bulletPref;
+
+	// player can not emit bullet when emitterColling is true
+	public bool cooling = false;
+
+	// bullet fire speed
+	public float bulletSpeedScale;
+
+	// Vector2.up or Vector.down
+	public Vector2 bulletDirection;
+
+	// once loadedBullet is set, it can be fired
+	private GameObject loadedBullet;
+
+	// cooling time
+	public float coolingSeconds;
+
+	// bullet tag
+	public string bulletTag;
+
+
+	public IEnumerator Fire() {
+		// enable cooling flag on fire
+		cooling = true;
+
+		// fire: add force to bullet
+		Rigidbody2D rigidbody = loadedBullet.GetComponent<Rigidbody2D> ();
+		rigidbody.AddForce (bulletDirection * bulletSpeedScale);
+
+		// destroy loaded bullet after 10s if it's fired.
+		Destroy (loadedBullet, 10.0f);
+
+		// set loadedBullet to null to make sure only one loaded bullet will be created.
+		loadedBullet.transform.parent = null;
+		loadedBullet = null;
+
+		// disable cooling flag after fire
+		yield return new WaitForSeconds (coolingSeconds);
+		cooling = false;
+	}
+
+	public bool NeedReload() {
+		// If emitter is cooling down, there is no need to reload
+		// If there is a loaded bullet, there is no need to reload
+		return !cooling && !loadedBullet;
+	}
+
+	public void Reload(){
+		// Load bullet at emitter position
+		loadedBullet = (GameObject) Instantiate (bulletPref, bulletEmitter.transform.position, Quaternion.identity);
+
+		loadedBullet.tag = bulletTag;
+		loadedBullet.transform.parent = gameObject.transform;
+	}
+
+	public bool CanFire(){
+		// emitter is cooled down
+		return !cooling;
+	}
+
+
+}
--- Assets/Scripts/GalagaBulletEmitter.cs
+++ Assets/Scripts/GalagaBulletEmitter.cs
@@ -1,60 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GalagaBulletEmitter : MonoBehaviour {
+public class GalagaBulletEmitter : BulletEmitter {
 
-	// emit bullet from this object
-	public GameObject bulletEmitter;
-
-	// bullet prefab referrence
-	public GameObject bulletPref;
-
-	// player can not emit bullet when emitterColling is true
-	public bool emitterCooling = false;
-
-	// bullet fire speed
-	public float bulletSpeedScale;
-
-	// once loadedBullet is set, it can be fired
-	private GameObject loadedBullet;
-
-	IEnumerator Fire() {
-		emitterCooling = true;
-
-		// fire: add force to bullet
-		Rigidbody2D rigidbody = loadedBullet.GetComponent<Rigidbody2D> ();
-		rigidbody.AddForce (Vector2.up * bulletSpeedScale);
-
-		// destroy loaded bullet after 10s if it's fired.
-		Destroy (loadedBullet, 10.0f);
-
-		// set loadedBullet to null to make sure only one loaded bullet will be created.
-		loadedBullet.transform.parent = null;
-		loadedBullet = null;
-
-		// cooling time: 1.5s
-		yield return new WaitForSeconds (1.5f);
-
-		emitterCooling = false;
-	}
-
-	bool NeedReload() {
-		// If emitter is cooling down, there is no need to reload
-		// If there is a loaded bullet, there is no need to reload
-		return !emitterCooling && !loadedBullet;
-	}
-
-	void Reload(){
-		// Load bullet at emitter position
-		loadedBullet = (GameObject) Instantiate (bulletPref, bulletEmitter.transform.position, Quaternion.identity);
-
-		loadedBullet.tag = "GalagaBullet";
-		loadedBullet.transform.parent = gameObject.transform;
-	}
-
-	bool CanFire(){
+	public new bool CanFire(){
 		// Player hit fire signal, and emitter is cooling down
-		return Input.GetKeyDown ("space") && !emitterCooling;
+		return Input.GetKeyDown ("space") && !cooling;
 	}
 
 	void Update () {
