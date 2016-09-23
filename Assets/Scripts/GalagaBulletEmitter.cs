0ebc6ec2ef4efe70efa4913f513d149eaa8f4f0b
soasme <soasme@gmail.com>
2016-09-23 14:06:47 +0800

    Limit fire limit for galaga bullet emitter.

--- Assets/Scripts/GalagaBulletEmitter.cs
+++ Assets/Scripts/GalagaBulletEmitter.cs
@@ -9,20 +9,61 @@ public class GalagaBulletEmitter : MonoBehaviour {
 	// bullet prefab referrence
 	public GameObject bulletPref;
 
+	// player can not emit bullet when emitterColling is true
+	public bool emitterCooling = false;
+
 	// bullet fire speed
 	public float bulletSpeedScale;
 
-	void Fire() {
-		GameObject bullet = (GameObject) Instantiate (bulletPref, bulletEmitter.transform.position, Quaternion.identity);
-		bullet.tag = "GalagaBullet";
-		Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D> ();
+	// once loadedBullet is set, it can be fired
+	private GameObject loadedBullet;
+
+	IEnumerator Fire() {
+		emitterCooling = true;
+
+		// fire: add force to bullet
+		Rigidbody2D rigidbody = loadedBullet.GetComponent<Rigidbody2D> ();
 		rigidbody.AddForce (Vector2.up * bulletSpeedScale);
-		Destroy (bullet, 10.0f);
+
+		// destroy loaded bullet after 10s if it's fired.
+		Destroy (loadedBullet, 10.0f);
+
+		// set loadedBullet to null to make sure only one loaded bullet will be created.
+		loadedBullet.transform.parent = null;
+		loadedBullet = null;
+
+		// cooling time: 1.5s
+		yield return new WaitForSeconds (1.5f);
+
+		emitterCooling = false;
+	}
+
+	bool NeedReload() {
+		// If emitter is cooling down, there is no need to reload
+		// If there is a loaded bullet, there is no need to reload
+		return !emitterCooling && !loadedBullet;
+	}
+
+	void Reload(){
+		// Load bullet at emitter position
+		loadedBullet = (GameObject) Instantiate (bulletPref, bulletEmitter.transform.position, Quaternion.identity);
+
+		loadedBullet.tag = "GalagaBullet";
+		loadedBullet.transform.parent = gameObject.transform;
+	}
+
+	bool CanFire(){
+		// Player hit fire signal, and emitter is cooling down
+		return Input.GetKeyDown ("space") && !emitterCooling;
 	}
 
 	void Update () {
-		if (Input.GetKeyDown ("space")) {
-			Fire ();
+		if (NeedReload()) {
+			Reload ();
+		}
+
+		if (CanFire()) {
+			StartCoroutine (Fire ());
 		}
 	}
 }
