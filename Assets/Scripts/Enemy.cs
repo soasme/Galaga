39236ca78c26a57d3f01c9188f51f92b49cc4077
soasme <soasme@gmail.com>
2016-09-23 14:16:01 +0800

    Move enemy health code into a module.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,39 +3,22 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	public GameObject explosionPref;
-	private Vector2 attackPosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.tag == "GalagaBullet" || collider.tag == "Galaga") {
-			PlayExplosion ();
-			Destroy (gameObject);
-		}
-	}
 
-	void PlayExplosion(){
-		var explosion = (GameObject)Instantiate (explosionPref);
-		explosion.transform.position = gameObject.transform.position;
-	}
 
 	public void MoveToPlayer(){
-		var galaga = GameObject.FindGameObjectWithTag ("Galaga");
-		if (galaga) {
-			Vector2 pos = transform.position;
-			var speed = 1.0f;
-			pos.y = pos.y - Time.deltaTime * speed;
-			pos.x = Mathf.Sin (pos.y) * 3;
-			transform.position = pos;
+		Vector2 pos = transform.position;
+
+		if (pos.y < -5.5f) {
+			pos.y = 5.5f;
 		}
+
+		var speed = 1.0f;
+		pos.y = pos.y - Time.deltaTime * speed;
+		pos.x = Mathf.Sin (pos.y) * 3;
+		transform.position = pos;
+
 	}
+
 }
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public GameObject explosionPref;
+	private Vector2 attackPosition;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.tag == "GalagaBullet" || collider.tag == "Galaga") {
+			PlayExplosion ();
+			Destroy (gameObject);
+		}
+	}
+
+	void PlayExplosion(){
+		var explosion = (GameObject)Instantiate (explosionPref);
+		explosion.transform.position = gameObject.transform.position;
+	}
+}
