c24a301718cbcffe5dd068b926444e807b5828ec
soasme <soasme@gmail.com>
2016-09-23 14:09:53 +0800

    Set galaga movement range.

--- Assets/Scripts/GalagaMovement.cs
+++ Assets/Scripts/GalagaMovement.cs
@@ -6,8 +6,13 @@ public class GalagaMovement : MonoBehaviour {
 	public float speed = 1f;
 
 	void Update () {
+		// get origin position
 		var position = gameObject.transform.position;
-		var x = position.x + Input.GetAxis ("Horizontal") * speed;
-		gameObject.transform.position = new Vector2 (x, position.y);
+
+		// add delta, and set range
+		gameObject.transform.position = new Vector2 (
+			Mathf.Clamp(position.x + Input.GetAxis ("Horizontal") * speed, -6f, 6f)
+			, position.y
+		);
 	}
 }
