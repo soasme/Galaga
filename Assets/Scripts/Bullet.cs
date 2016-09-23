ed751755263769f3b4bf62d84555fdbefb63e924
soasme <soasme@gmail.com>
2016-09-23 14:12:01 +0800

    remove useless code.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (this.tag == "GalagaBullet" && collider.tag == "Enemy" ||
 			this.tag == "EnemyBullet" && collider.tag == "Galaga"
